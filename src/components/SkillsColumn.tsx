d26615c017fb9749764e358e4b7c09315fc7e1a0
Raymond Wolt <rjwolt@gmail.com>
2023-10-03 23:04:07 -0500

    Add content to project pages

--- src/components/SkillsColumn.tsx
+++ src/components/SkillsColumn.tsx
@@ -2,13 +2,13 @@ import styles from "../components/SkillsColumn.module.css";
 import { Icon } from "astro-icon";
 
 type SkillsProps = {
-  skills: [string, string][];
+  skills?: [string, string][];
 };
 
 const SkillsColumn = ({ skills }: SkillsProps) => {
   return (
     <ul>
-      {skills.map(([skillName, iconName], index) => (
+      {skills?.map(([skillName, iconName], index) => (
         <div className={styles.skillContainer}>
           <img src={`/icons/${iconName}.svg`} alt={`${skillName} logo`} />
           <li key={index}>{skillName}</li>
